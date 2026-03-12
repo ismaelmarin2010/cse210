da66db2a3880aea1d385ef4871b27562a55fec00
ismaelmarin2010 <65473600+ismaelmarin2010@users.noreply.github.com>
2026-03-11 21:55:09 -0300

    Excersices for week 1 for wdd131

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,41 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("");
-        Console.WriteLine("Hello Handsome! This is the Exercise3 Project.");
+        Console.WriteLine("This is the Exercise3 Project.");
         Console.WriteLine("-------------------------------------------");
         Console.WriteLine("");
 
-        // What is the magic number? 6
-        // What is your guess? 4
-        // Higher
+        // Request the user for the magic number for Parts 1 and 2
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
 
-        // Use the random class to generate a number between two others.
+        // Generate a random number for Part 3
         Random random = new Random();
         int magicNumber = random.Next(1, 101);
 
-        Console.WriteLine("A random number between 1 and 10 was generated:");
-        Console.WriteLine(magicNumber);
-
-
-
         // Initialize the variable that is used in the loop.
-        int guessNumber = 0;
+        int guessNumber = -1;
 
         do
         {
             // Print screen requesting the user for a number.
             Console.Write("What is your guess? ");
 
-            // Capture the number entered by the user (string)
-            string strGuessNumber = Console.ReadLine();
-
             // Take the number entered and save it as int (variable pre initialized)
-            guessNumber = int.Parse(strGuessNumber);
+            guessNumber = int.Parse(Console.ReadLine());
 
             // Do the verification to check if the user guessed
-            if (guessNumber == magicNumber)
+            if (guessNumber < magicNumber)
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine("Higher");
             }
 
             else if (guessNumber > magicNumber)
@@ -49,15 +41,12 @@ class Program
 
             else
             {
-                Console.WriteLine("Higher");
+                Console.WriteLine("You guessed it!");
             }
-
-            // Add a line to improve readability
-            Console.WriteLine("");
 
         } while (guessNumber != magicNumber);
 
-        
+
 
 
 
